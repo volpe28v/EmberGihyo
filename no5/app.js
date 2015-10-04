3ffea54c25975f146cc254994e2fc49a8e04a4af
naoki kodama <naoki.koda@gmail.com>
2015-10-04 23:40:17 +0900

    localStorage に保存、リストア可能とした

--- no5/app.js
+++ no5/app.js
@@ -10,23 +10,7 @@ App.Router.map(function() {
 
 App.ProductsRoute = Ember.Route.extend({
   model: function(){
-    return [{
-      id: 1,
-      name: 'ステッカー',
-      price: 6.0,
-      url: 'http://devswag.com/products/ember-sticker'
-    },{
-      id: 2,
-      name: 'Tシャツ',
-      price: 22.0,
-      url: 'http://devswag.com/products/ember-js-tshirt'
-    },{
-      id: 3,
-      name: 'ぬいぐるみ',
-      price: 10.0,
-      url: 'http://devswag.com/products/ember-mascot-tomster'
-    }
-  ];
+    return App.Product.all();
   }
 /*
   // こっちに定義しても動く
@@ -48,6 +32,7 @@ App.ProductsShowRoute = Ember.Route.extend({
   actions: {
     addCart: function(product){
       this.controllerFor('cart').pushObject(product);
+      this.controllerFor('cart').save();
       this.transitionTo('cart');
       console.log("ProductsShowRoute");
     }
@@ -64,5 +49,75 @@ App.CartController = Ember.ArrayController.extend({
     return this.mapBy('price').reduce(function(total, price) {
       return total + price;
     }, 0);
-  }.property('@each.price') // length でも動くと思う
+  }.property('@each.price'), // length でも動くと思う
+
+  save: function(){
+    var ids = JSON.stringify(this.mapBy('id'));
+    localStorage.setItem('cart-product-ids', ids);
+  },
+
+  restore: function(){
+    var idsString = localStorage.getItem('cart-product-ids');
+    var ids;
+    if (idsString) {
+      ids = JSON.parse(idsString);
+    } else {
+      ids = [];
+    }
+    var products = ids.map(function(id) {
+      return App.Product.find(id);
+    });
+    products = products.compact();
+    this.set('model', products);
+  }
 });
+
+
+App.Product = Ember.Object.extend({
+  id: null,
+  name: null,
+  price: 0,
+  url: null
+});
+
+App.Product.reopenClass({
+  data: [],
+
+  all: function(){
+    return App.Product.data;
+  },
+
+  find: function(id){
+    return this.all().findBy('id', Number(id));
+  }
+});
+
+App.Product.data.pushObjects([
+  App.Product.create({
+    id: 1,
+    name: 'ステッカー',
+    price: 6.0,
+    url: 'http://devswag.com/products/ember-sticker'
+  }),
+  App.Product.create({
+    id: 2,
+    name: 'Tシャツ',
+    price: 22.0,
+    url: 'http://devswag.com/products/ember-js-tshirt'
+  }),
+  App.Product.create({
+    id: 3,
+    name: 'ぬいぐるみ',
+    price: 10.0,
+    url: 'http://devswag.com/products/ember-mascot-tomster'
+  })
+]);
+
+App.initializer({
+  name: 'restore-cart',
+
+  initialize: function(container, app){
+    container.lookup('controller:cart').restore();
+  }
+});
+
